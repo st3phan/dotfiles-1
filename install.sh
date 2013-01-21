82f54e64a8651dff8966f834c143079c1c1fd047
Vítor Galvão <info@vitorgalvao.com>
2013-01-21 16:04:29 +0000

    Added homebrew to install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@
 curl -L https://raw.github.com/vitorgalvao/dotfiles/master/scripts/osx.sh | sh
 curl -L https://raw.github.com/vitorgalvao/dotfiles/master/scripts/zsh.sh | sh
 curl -L https://raw.github.com/vitorgalvao/dotfiles/master/scripts/vim.sh | sh
+curl -L https://raw.github.com/vitorgalvao/dotfiles/master/scripts/homebrew.sh | sh
 curl -L https://raw.github.com/vitorgalvao/dotfiles/master/scripts/app_specific_configs.sh | sh
