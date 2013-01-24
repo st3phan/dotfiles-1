7e24fac8471873895309dbbb4bfcf0ec6b254748
Vítor Galvão <info@vitorgalvao.com>
2013-01-24 17:50:20 +0000

    Changed install order

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curl -L https://raw.github.com/vitorgalvao/dotfiles/master/scripts/osx.sh | sh
+curl -L https://raw.github.com/vitorgalvao/dotfiles/master/scripts/homebrew.sh | sh
 curl -L https://raw.github.com/vitorgalvao/dotfiles/master/scripts/zsh.sh | sh
 curl -L https://raw.github.com/vitorgalvao/dotfiles/master/scripts/vim.sh | sh
-curl -L https://raw.github.com/vitorgalvao/dotfiles/master/scripts/homebrew.sh | sh
 curl -L https://raw.github.com/vitorgalvao/dotfiles/master/scripts/app_specific_configs.sh | sh
