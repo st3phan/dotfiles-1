6aae4fc211b2bf41cfb5b87fa0291ecae98544e0
Vítor Galvão <info@vitorgalvao.com>
2013-01-28 02:22:04 +0000

    SublimeText2 CLI

--- scripts/other_installs.sh
+++ scripts/other_installs.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-# Get cputhrottle
-curl "http://www.willnolan.com/cputhrottle/cputhrottle.gz" | gunzip > /usr/local/bin/cputhrottle
+##############################
+### cputhrottle            ###
+##############################
+curl 'http://www.willnolan.com/cputhrottle/cputhrottle.gz' | gunzip > /usr/local/bin/cputhrottle
 chmod +x /usr/local/bin/cputhrottle
+
+##############################
+### SublimeTex2 CLI        ###
+##############################
+ln -s '/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl' ~/usr/local/bin/subl
