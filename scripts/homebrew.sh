7a6aea10c308b62ae23f0aea443e02f58f10419f
Vítor Galvão <info@vitorgalvao.com>
2013-01-28 15:18:00 +0000

    Clarified homebrew script

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+##############################
+### Homebrew               ###
+##############################
 # Install homebrew
 ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
 
@@ -8,6 +11,9 @@ brew install atool lftp mplayer trash tree vim
 brew install imagemagick --use-png --use-tiff
 brew install zsh --disable-etcdir
 
+##############################
+### Brew cask              ###
+##############################
 # Install brew cask
 brew tap phinze/homebrew-cask
 brew install brew-cask
