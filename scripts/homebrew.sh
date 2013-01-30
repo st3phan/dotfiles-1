6f308a5c2805ea8e5bb940a27366f3c04a4e5628
Vítor Galvão <info@vitorgalvao.com>
2013-01-30 20:11:27 +0000

    Refined brew-cask installs

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -18,7 +18,20 @@ brew install zsh --disable-etcdir
 brew tap phinze/homebrew-cask
 brew install brew-cask
 
-# Install apps and move them to /Applications
-export HOMEBREW_CASK_OPTS="--appdir=/Applications"
-brew cask install alfred github google-chrome keka skype sublime-text transmission
-brew cask linkapps
+# Install apps
+APPS_TO_INSTALL="alfred dropbox github google-chrome keka skype sublime-text transmission"
+brew cask install ${APPS_TO_INSTALL}
+
+# Move .app files to /Applications/
+for APP in $(echo ${APPS_TO_INSTALL})
+do
+	mv /usr/local/Cellar/${APP}/*/*.app /Applications/
+done
+
+# Remove leftovers from /usr/bin/Cellar
+brew cask uninstall ${APPS_TO_INSTALL}
+
+for APP in $(echo ${APPS_TO_INSTALL})
+do
+	mv /usr/local/Cellar/${APP}/ ${HOME}/.Trash/
+done
