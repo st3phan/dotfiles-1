6cdb414188da124c1323e7d689d66ea4b30c284b
Vítor Galvão <info@vitorgalvao.com>
2013-02-26 20:57:38 +0000

    Added personal scripts

--- scripts/other_installs.sh
+++ scripts/other_installs.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 ##############################
+### my personal scripts    ###
+##############################
+git clone https://github.com/vitorgalvao/tiny-scripts.git /tmp/tiny-scripts/
+rm /tmp/tiny-scripts/README.md
+chmod +x /tmp/tiny-scripts/*
+mv /tmp/tiny-scripts/ /usr/local/bin/
+
+##############################
 ### cputhrottle            ###
 ##############################
 curl 'http://www.willnolan.com/cputhrottle/cputhrottle.gz' | gunzip > /usr/local/bin/cputhrottle
@@ -11,6 +19,5 @@ chmod +x /usr/local/bin/cputhrottle
 ##############################
 # Call from CLI
 ln -s '/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl' /usr/local/bin/subl
-
 # Install personal preferences
 mv "/tmp/dotfiles/files/Preferences.sublime-settings" "${HOME}/Library/Application Support/Sublime Text 2/Packages/User/"
