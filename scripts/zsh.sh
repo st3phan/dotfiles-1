b98fc4432147e72d3a19da34c427be6ba7148247
Vítor Galvão <info@vitorgalvao.com>
2013-03-13 14:24:25 +0000

    fixed a comment typo

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -6,7 +6,7 @@ chsh -s /usr/local/bin/zsh
 # install oh-my-zsh
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 
-# intall zsh-syntax-highlighting
+# install zsh-syntax-highlighting
 mkdir -p ${HOME}/.oh-my-zsh/custom/plugins
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ${HOME}/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
