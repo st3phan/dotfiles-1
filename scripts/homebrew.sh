15882987d6d892fe970f2e7b1347de8fcff725b8
Vítor Galvão <info@vitorgalvao.com>
2013-05-02 01:04:03 +0100

    Added steam and path-finder, removed xtra-finder and sublime-text

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -24,7 +24,7 @@ brew tap phinze/homebrew-cask
 brew install brew-cask
 
 # Install apps
-APPS_TO_INSTALL="alfred avidemux dropbox f-lux google-chrome google-chrome-canary keka macvim mplayerx skype spectacle sublime-text u-torrent xtra-finder"
+APPS_TO_INSTALL="alfred avidemux dropbox f-lux google-chrome google-chrome-canary keka macvim mplayerx path-finder skype spectacle steam u-torrent" 
 brew cask install ${APPS_TO_INSTALL}
 
 # Move .app files to /Applications/
