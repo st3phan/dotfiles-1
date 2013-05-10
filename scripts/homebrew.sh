f7ef2e122254a29879e386214aeee1281544ac21
Vítor Galvão <info@vitorgalvao.com>
2013-05-10 17:29:45 +0100

    macvim is not handled by homebrew and cask will now organize its apps

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -7,7 +7,7 @@
 ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
 
 # Install apps
-brew install atool cpulimit iterm2 lftp mediainfo mkvtoolnix mplayer trash tree vim
+brew install atool cpulimit iterm2 lftp macvim mediainfo mkvtoolnix mplayer trash tree vim
 brew install git --without-completions
 brew install imagemagick --with-libtiff --with-librsvg
 brew install ffmpeg --with-libvpx --with-libvorbins --with-theora
@@ -16,33 +16,12 @@ brew install zsh --disable-etcdir
 ##############################
 ### Brew cask              ###
 ##############################
-# Caskroom directory
-CASKROOM="/opt/homebrew-cask/Caskroom"
-
 # Install brew cask
 brew tap phinze/homebrew-cask
 brew install brew-cask
 
 # Install apps
-APPS_TO_INSTALL="air-server alfred avidemux dropbox f-lux google-chrome google-chrome-canary instacast keka macvim mplayerx path-finder processing skype spectacle steam u-torrent xscope"
-brew cask install ${APPS_TO_INSTALL}
-
-# Move .app files to /Applications/
-for APP in ${APPS_TO_INSTALL}
-do
-	find "${CASKROOM}/${APP}" -iname "${APP}.app" -print0 | xargs -0 -I {} mv {} /Applications/
-done
-
-# Get mvim
-find ${CASKROOM}/macvim -iname "mvim" -exec mv -v "{}" /usr/local/bin/ \;
-
-# Remove leftovers from Caskroom's directory
-brew cask uninstall ${APPS_TO_INSTALL}
-
-for APP in ${APPS_TO_INSTALL}
-do
-	mv ${CASKROOM}/${APP}/ ${HOME}/.Trash/
-done
+brew cask install air-server alfred avidemux dropbox f-lux google-chrome google-chrome-canary instacast keka mplayerx path-finder processing skype spectacle steam u-torrent xscope
 
 ##############################
 ### Cleanup cache          ###
