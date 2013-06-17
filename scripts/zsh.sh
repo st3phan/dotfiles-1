7db4b770a835f3246068e74f602e0f4c353d7e58
Vítor Galvão <info@vitorgalvao.com>
2013-06-17 20:47:50 +0100

    more paths in quotes

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # make default shell
 sudo sh -c "echo '/usr/local/bin/zsh' >> /etc/shells"
-chsh -s /usr/local/bin/zsh
+chsh -s "/usr/local/bin/zsh"
 
 # install oh-my-zsh
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 
 # install zsh-syntax-highlighting
-mkdir -p ${HOME}/.oh-my-zsh/custom/plugins
+mkdir -p "${HOME}/.oh-my-zsh/custom/plugins"
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ${HOME}/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 # add 'oneend' theme
