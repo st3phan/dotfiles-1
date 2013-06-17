8141a152a7458d4792ac793388cf2ae0d87eef0f
Vítor Galvão <info@vitorgalvao.com>
2013-06-17 20:46:29 +0100

    refine install, it now asks for developer tools beforehand

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+open "http://developer.apple.com/downloads"
+read -p "Homebrew needs Xcode's CLI tools. Download them and press return to continue"
+
 git clone https://github.com/vitorgalvao/dotfiles.git /tmp/dotfiles/
 
-cat /tmp/dotfiles/osx.sh | sh
-cat /tmp/dotfiles/homebrew.sh | sh
-cat /tmp/dotfiles/zsh.sh | sh
-cat /tmp/dotfiles/vim.sh | sh
-cat /tmp/dotfiles/other_installs.sh | sh
-cat /tmp/dotfiles/app_specific_configs.sh | sh
+cat /tmp/dotfiles/scripts/osx.sh | sh
+cat /tmp/dotfiles/scripts/homebrew.sh | sh
+cat /tmp/dotfiles/scripts/zsh.sh | sh
+cat /tmp/dotfiles/scripts/vim.sh | sh
+cat /tmp/dotfiles/scripts/other_installs.sh | sh
+cat /tmp/dotfiles/scripts/app_specific_configs.sh | sh
 
-mv /tmp/dotfiles/manual/ ${HOME}/Desktop/
+mv "/tmp/dotfiles/manual/" "${HOME}/Desktop/"
