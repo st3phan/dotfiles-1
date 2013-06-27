2ad48635803b4efb6d8c422adfe3d6e8ff302173
Vítor Galvão <info@vitorgalvao.com>
2013-06-27 15:47:57 +0100

    'make default shell' is now later in the script

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# make default shell
-sudo sh -c "echo '/usr/local/bin/zsh' >> /etc/shells"
-chsh -s "/usr/local/bin/zsh"
-
 # install oh-my-zsh
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 
@@ -12,6 +8,10 @@ git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ${HOME}/.oh-my-
 
 # add 'oneend' theme
 mv "/tmp/dotfiles/files/oneend.zsh-theme" "${HOME}/.oh-my-zsh/themes/"
+
+# make default shell
+sudo sh -c "echo '/usr/local/bin/zsh' >> /etc/shells"
+chsh -s "/usr/local/bin/zsh"
 
 # dotfiles
 mv "/tmp/dotfiles/files/zshrc" "${HOME}/.zshrc"
