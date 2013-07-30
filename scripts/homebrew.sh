56c30c51bbbe375056c046bf46f2d5a852eea223
Vítor Galvão <info@vitorgalvao.com>
2013-07-30 19:04:18 +0100

    macvim and vim compiled with lua support

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -12,7 +12,12 @@ brew install avidemux --with-qt
 brew install git --without-completions
 brew install imagemagick --with-libtiff --with-librsvg
 brew install ffmpeg --with-libvpx --with-libvorbins --with-theora
+brew install macvim --with-lua
+brew install vim --with-lua
 brew install zsh --disable-etcdir
+
+# Move MacVim to /Applications/
+mv /usr/local/Cellar/macvim/*/MacVim.app /Applications/
 
 ##############################
 ### Brew cask              ###
@@ -23,15 +28,12 @@ brew tap phinze/homebrew-cask
 brew install brew-cask
 
 # Install apps
-appsToInstall="air-server alfred codekit dropbox f-lux google-chrome google-chrome-canary google-hangouts instacast instashare iterm2 keka key-codes macvim mplayerx node-webkit pokerstars processing skype spectacle steam textexpander u-torrent xscope"
+appsToInstall="air-server alfred codekit dropbox f-lux google-chrome google-chrome-canary google-hangouts instacast instashare iterm2 keka key-codes mplayerx node-webkit pokerstars processing skype spectacle steam textexpander u-torrent xscope"
 
 brew cask install ${appsToInstall}
 
 # Copy apps to /Applications/
 readlink ${HOME}/Applications/* | xargs -I {} cp -R {} /Applications/
-
-# Get mvim
-find ${caskroom} -iname 'mvim' -exec cp {} "/usr/local/bin/" \;
 
 # Remove them from homebrew-cask's directory
 brew cask uninstall ${appsToInstall}
