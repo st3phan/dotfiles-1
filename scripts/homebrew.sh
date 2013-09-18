915b464b591d9500a9cd487e8ac8fef9bc5a8c65
Vítor Galvão <info@vitorgalvao.com>
2013-09-18 18:00:47 +0100

    added shotcut

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -31,7 +31,7 @@ brew tap phinze/homebrew-cask
 brew install brew-cask
 
 # Install apps
-appsToInstall="air-server alfred codekit dropbox f-lux google-chrome google-chrome-canary google-hangouts instacast iterm2 keka key-codes licecap mplayerx node-webkit platypus pokerstars processing skype spectacle steam textexpander u-torrent xscope"
+appsToInstall="air-server alfred codekit dropbox f-lux google-chrome google-chrome-canary google-hangouts instacast iterm2 keka key-codes licecap mplayerx node-webkit platypus pokerstars processing shotcut skype spectacle steam textexpander u-torrent xscope"
 
 brew cask install ${appsToInstall}
 
