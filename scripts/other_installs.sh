02e69f91a7209747be0a213f1da5f6a7c249bceb
Vítor Galvão <info@vitorgalvao.com>
2013-10-05 22:55:29 +0100

    change MPlayer2 icons

--- scripts/other_installs.sh
+++ scripts/other_installs.sh
@@ -30,6 +30,18 @@ unzip /tmp/SourceCodePro.zip -d /tmp/
 mv /tmp/SourceCodePro_FontsOnly*/OTF/SourceCodePro-Regular.otf /Library/Fonts/
 
 ##############################
+### Change MPlayer2 icon   ###
+##############################
+cd /tmp/
+curl 'http://fc04.deviantart.net/fs71/f/2012/237/8/9/mplayerx_replacements_by_manyk-d5499gq.zip' -o mplayerx-icons.zip
+unzip mplayerx-icons.zip
+mv MPlayerX.icns /Applications/MPlayer2.app/Contents/Resources/icon.icns
+mv doc_video.icns /Applications/MPlayer2.app/Contents/Resources/movie.icns
+mv doc_audio.icns /Applications/MPlayer2.app/Contents/Resources/audio.icns
+mv doc_sub.icns /Applications/MPlayer2.app/Contents/Resources/subtitles.icns
+cd -
+
+##############################
 ### qlImageSize            ###
 ##############################
 cd /tmp/
