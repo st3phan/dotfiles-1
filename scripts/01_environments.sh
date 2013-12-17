420e0c6be5a25c49a96a851d9fa905f1de68210b
Vítor Galvão <info@vitorgalvao.com>
2013-12-17 03:19:34 +0000

    install python and ruby with homebrew

--- scripts/01_environments.sh
+++ scripts/01_environments.sh
@@ -2,24 +2,13 @@
 ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go/install)"
 
 # python environment
-brew install pyenv
-pythonVersion=$(pyenv install --list | sed 's/^  //' | grep '^2' | grep --invert-match 'dev' | tail -1)
-pyenv install ${pythonVersion}
-# sets the version to use, to install packages now
-pyenv global ${pythonVersion}
-if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
-# install docutils (will be needed for mpv)
-pip install docutils
-# install some eggs
-pip install --upgrade asciinema
+brew install python
+# install some eggs (docutils will be needed for mpv)
+pip install asciinema docutils
 
 # ruby environment
-brew install chruby ruby-install
-ruby-install ruby # install latest ruby
-rubyVersion=$(ruby-install | grep 'stable' | head -1 | sed 's/.*: //')
-# sets the version to use, to install packages now
-source /usr/local/share/chruby/chruby.sh
-chruby ruby-${rubyVersion}
+brew install ruby
+echo "gem: -n/usr/local/bin" >> ~/.gemrc
 # install some gems
 gem install bundler watir-webdriver chromedriver2-helper
 
@@ -27,3 +16,6 @@ gem install bundler watir-webdriver chromedriver2-helper
 brew install node
 # install some packages
 npm install -g browser-sync bower grunt-cli harp imageoptim-cli
+
+# make sure python and ruby are in the PATH
+export PATH="/usr/local/bin:$PATH"
