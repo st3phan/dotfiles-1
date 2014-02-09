e19bac6ca134a49724c54920b8e19d438961a812
Vítor Galvão <info@vitorgalvao.com>
2014-02-09 16:47:18 +0000

    removed google hangouts

--- scripts/02_apps.sh
+++ scripts/02_apps.sh
@@ -24,7 +24,7 @@ mkdir -p /opt/homebrew-cask/Caskroom
 sudo chown -R ${USER}:staff /opt/homebrew-cask
 
 # install apps
-brew cask install --appdir="/Applications" airserver alfred apikitchen bartender bettertouchtool dropbox enjoyable f-lux google-chrome google-hangouts haroopad imageoptim instacast iterm2 join-me keka key-codes node-webkit openemu platypus pokerstars processing shiori shotcut skype spectacle steam sublime-text textexpander transmission
+brew cask install --appdir="/Applications" airserver alfred apikitchen bartender bettertouchtool dropbox enjoyable f-lux google-chrome haroopad imageoptim instacast iterm2 join-me keka key-codes node-webkit openemu platypus pokerstars processing shiori shotcut skype spectacle steam sublime-text textexpander transmission
 
 # drivers
 brew cask install xbox360-controller-driver
