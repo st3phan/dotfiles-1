148008f8fb4df8087e6a9555b3ad1682d3cf95ec
Vítor Galvão <info@vitorgalvao.com>
2014-03-21 18:26:46 +0000

    changed git install command

--- scripts/02_apps.sh
+++ scripts/02_apps.sh
@@ -1,6 +1,5 @@
 # homebrew apps
-brew install atool cpulimit elinks git-extras git-ftp hr lftp mackup mediainfo mkvtoolnix mp4box namebench phantomjs trash tree z
-brew install git --without-completions
+brew install atool cpulimit elinks git git-extras git-ftp hr lftp mackup mediainfo mkvtoolnix mp4box namebench phantomjs trash tree z
 brew install imagemagick --with-libtiff
 brew install ffmpeg --with-libvpx --with-libvorbis --with-theora
 brew install macvim --with-lua --custom-icons
