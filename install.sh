6a127a3930fd2f886d47adba4db241886ac7bccc
Vítor Galvão <info@vitorgalvao.com>
2014-04-12 19:41:33 +0100

    make concatenation and running separate

--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@ read -p "Homebrew needs Xcode's CLI tools. Download and install them and press R
 git clone https://github.com/vitorgalvao/dotfiles.git /tmp/dotfiles/
 
 # concatenate all parts together, so things like variables can be reused
-cat /tmp/dotfiles/scripts/* | sh
+cat /tmp/dotfiles/scripts/* > /tmp/script
+bash /tmp/script
 
 mv "/tmp/dotfiles/files/" "${HOME}/Desktop/"
 
