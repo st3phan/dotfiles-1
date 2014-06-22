3005937e20f660b6d1860610bf639c4f52e0c643
Vítor Galvão <info@vitorgalvao.com>
2014-06-22 13:42:18 +0100

    don't rearrange spaces

--- scripts/03_osx.sh
+++ scripts/03_osx.sh
@@ -142,6 +142,12 @@ defaults write com.apple.dock magnification -bool false
 # Disable the Dashboard
 defaults write com.apple.dashboard mcx-disabled -bool true
 
+# Don’t show Dashboard as a Space
+defaults write com.apple.dock dashboard-in-overlay -bool true
+
+# Don’t automatically rearrange Spaces based on most recent use
+defaults write com.apple.dock mru-spaces -bool false
+
 # Hot corners
 # Possible values:
 #  0: no-op
