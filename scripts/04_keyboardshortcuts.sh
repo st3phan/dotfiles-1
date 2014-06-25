0a9f15e3b256b38e132efc22874f086882503478
Vítor Galvão <info@vitorgalvao.com>
2014-06-25 14:56:23 +0100

    added keyboardshortcuts script

---
+++ scripts/04_keyboardshortcuts.sh
@@ -0,0 +1,10 @@
+# Custom keyboard shortcuts
+# @ is ⌘; ~ is ⌥; $ is ⇧; ^ is ^
+# read more at http://hints.macworld.com/article.php?story=20131123074223584
+
+# Instacast
+defaults write com.vemedio.osx.Instacast NSUserKeyEquivalents '{
+  "Mark as Played"="~p";
+  "Mark as Unplayed"="~u";
+  "Download"="~d";
+}'
