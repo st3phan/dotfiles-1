95663533acdac044354b0d4da363eb0330b043f4
Vítor Galvão <info@vitorgalvao.com>
2014-06-27 16:00:26 +0100

    added prfix

--- scripts/02_apps.sh
+++ scripts/02_apps.sh
@@ -47,7 +47,7 @@ brew cask install colorpicker-antetype colorpicker-skalacolor
 # my personal taps
 # tiny-scripts
 brew tap vitorgalvao/homebrew-tinyscripts
-brew install cask-repair casks-audit crafts customise-terminal-notifier dropboxtimer gfv gifmaker labelcolor lovecolor olx-post pedir-gas pinboardlinkcheck podbook seren unsplashdownload
+brew install cask-repair casks-audit crafts customise-terminal-notifier dropboxtimer gfv gifmaker labelcolor lovecolor olx-post pedir-gas pinboardlinkcheck podbook prfix seren unsplashdownload
 
 # cleanup homebrew's cache
 brew cleanup --force -s
