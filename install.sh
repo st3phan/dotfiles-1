baf4bec07ba6f585952b0d0e8bc266feaaff3c32
Vítor Galvão <info@vitorgalvao.com>
2014-08-27 22:01:52 +0100

    removed instructions to download xcode cli tools

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-open "http://developer.apple.com/downloads"
-read -p "Homebrew needs Xcode's CLI tools. Download and install them and press RETURN to continue"
-
 git clone https://github.com/vitorgalvao/dotfiles.git /tmp/dotfiles/
 
 # concatenate all parts together, so things like variables can be reused
