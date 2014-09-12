6b8f26745639fa4aa311e05acf65291780c8dbe4
Vítor Galvão <info@vitorgalvao.com>
2014-09-12 20:58:42 +0100

    install now only concatenates shell scripts

--- install.sh
+++ install.sh
@@ -2,8 +2,12 @@
 
 git clone https://github.com/vitorgalvao/dotfiles.git /tmp/dotfiles/
 
-# concatenate all parts together, so things like variables can be reused
-cat /tmp/dotfiles/scripts/* > /tmp/script
+# concatenate all shell scripts together, so things like variables can be reused
+cat /tmp/dotfiles/scripts/*.sh > /tmp/script
 bash /tmp/script
 
+# run ruby scripts
+ruby /tmp/dotfiles/scripts/chrome_extensions.rb
+
+# move files that'll need manual action
 mv "/tmp/dotfiles/files/" "${HOME}/Desktop/"
