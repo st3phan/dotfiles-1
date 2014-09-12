59175a51e96797731c8dcf87f41c3659afd5fa2a
Vítor Galvão <info@vitorgalvao.com>
2014-09-12 21:02:06 +0100

    added script to load chrome extensions

---
+++ scripts/07_chrome_extensions.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+chrome_extensions = {
+  :onepassword => 'aomjjhallfgjeglblehebfpbcfeobpgk',
+  :adblockplus => 'cfhdojbkjhnklbpkdaibdccddilifddb',
+  :buffer => 'noojglkidnpfjbincgijbaiedldjfbhh',
+  :momentum => 'laookkfknpbbblfpciffpaejjkokdgca',
+  :vimium => 'dbepggeogbaibhgnhhndojpepiihcmeb'
+}
+
+canary_extensions = {
+  :chromeextensionsdeveloper => 'ohmmkhmmmpcnpikjeljgnaoabkaalbgc',
+  :emmetlivestyle => 'diebikgmpmeppiilkaijjbdgciafajmg',
+  :kimono => 'deoaddaobnieaecelinfdllcgdehimih',
+  :livereload => 'jnihajbhpnppcggbcgedagnkighmdlei',
+  :snappysnippet => 'blfngdefapoapkcdibbdkigpeaffgcil',
+  :tincr => 'lfjbhpnjiajjgnjganiaggebdhhpnbih'
+}
+
+chrome_executable = %x(mdfind kMDItemCFBundleIdentifier = com.google.Chrome).strip + '/Contents/MacOS/Google Chrome'
+canary_executable = %x(mdfind kMDItemCFBundleIdentifier = com.google.Chrome.canary).strip + '/Contents/MacOS/Google Chrome Canary'
+
+chrome_extensions.each do |key, value|
+  system(chrome_executable, '--no-first-run', '--disable-default-apps', "--install-from-webstore=#{value}")
+end
+
+canary_extensions.each do |key, value|
+  system(canary_executable, '--no-first-run', '--disable-default-apps', "--install-from-webstore=#{value}")
+end
