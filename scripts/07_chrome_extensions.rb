f95a535c623cb6bf4cdd28548b6f7188818303cd
Vítor Galvão <info@vitorgalvao.com>
2015-01-09 18:57:31 +0000

    added hola do canary

--- scripts/07_chrome_extensions.rb
+++ scripts/07_chrome_extensions.rb
@@ -14,6 +14,7 @@ chrome_extensions = {
 canary_extensions = {
   :chromeextensionsdeveloper => 'ohmmkhmmmpcnpikjeljgnaoabkaalbgc',
   :emmetlivestyle => 'diebikgmpmeppiilkaijjbdgciafajmg',
+  :holabetterinternet => 'gkojfkhlekighikafcpjkiklfbnlmeio',
   :livereload => 'jnihajbhpnppcggbcgedagnkighmdlei',
   :snappysnippet => 'blfngdefapoapkcdibbdkigpeaffgcil',
   :tincr => 'lfjbhpnjiajjgnjganiaggebdhhpnbih'
