89b3e545022bebb4cbc474dab89ebf19e162666f
Vítor Galvão <info@vitorgalvao.com>
2015-01-13 22:52:07 +0000

    added global shortcuts for pasting

--- scripts/04_keyboardshortcuts.sh
+++ scripts/04_keyboardshortcuts.sh
@@ -2,6 +2,12 @@
 # @ is ⌘; ~ is ⌥; $ is ⇧; ^ is ⌃
 # read more at https://web.archive.org/web/20140810142907/http://hints.macworld.com/article.php?story=20131123074223584
 
+# Global
+defaults write -g NSUserKeyEquivalents '{
+  "Paste and Match Style"="@v";
+  "Paste"="@~$v";
+}'
+
 # Instacast
 defaults write com.vemedio.osx.Instacast NSUserKeyEquivalents '{
   "Mark as Played"="\b";
