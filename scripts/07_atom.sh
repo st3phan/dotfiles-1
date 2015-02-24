fd606a698e8524687dfe81a47d23d07600afcb5d
Vítor Galvão <info@vitorgalvao.com>
2015-02-24 16:21:11 +0100

    added atom script

---
+++ scripts/07_atom.sh
@@ -0,0 +1,8 @@
+# packages
+apm install atom-css-comb autocomplete-plus autocomplete-snippets color-picker dash git-plus javascript-snippets language-git language-jade linter linter-ruby linter-shellcheck monokai vim-mode
+
+# themes
+apm install spacegray-dark-ui
+
+# syntaxes
+apm install peacock-syntax
