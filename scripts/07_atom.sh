d2b679192ef9f31c39ca028a88ce63ab3f233dbf
Vítor Galvão <info@vitorgalvao.com>
2015-03-14 02:13:11 +0100

    -linter-ruby +linter-rubocop

--- scripts/07_atom.sh
+++ scripts/07_atom.sh
@@ -1,5 +1,5 @@
 # packages
-apm install atom-css-comb autocomplete-plus autocomplete-snippets color-picker dash git-plus javascript-snippets language-git language-jade linter linter-ruby linter-shellcheck monokai processing-language vim-mode
+apm install atom-css-comb autocomplete-plus autocomplete-snippets color-picker dash git-plus javascript-snippets language-git language-jade linter linter-rubocop linter-shellcheck monokai processing-language vim-mode
 
 # themes
 apm install spacegray-dark-ui
