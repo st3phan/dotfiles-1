6db23c9a8dd6e4711def4829ae1db3a809ca8714
Vítor Galvão <info@vitorgalvao.com>
2015-04-16 23:31:31 +0100

    python and ruby no longer managed by homebrew

--- scripts/01_environments.sh
+++ scripts/01_environments.sh
@@ -8,20 +8,27 @@ softwareupdate --install --all
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # python environment
-brew install python
+brew install pyenv
+python_version=$(pyenv install --list | tr -d ' ' | grep '^\d' | grep --invert-match 'dev\|a' | tail -1)
+pyenv install "${python_version}"
+if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
+pyenv global "${python_version}"
 # install some eggs
 pip install livestreamer subliminal
 
 # ruby environment
-brew install ruby
+brew install chruby ruby-install
+ruby-install ruby
+source "/usr/local/share/chruby/chruby.sh"
+chruby ruby
 # install some gems
-export GEM_HOME="$(brew --prefix)"
 gem install --no-ri --no-rdoc bundler chromedriver2-helper pry redcarpet ronn rubocop scss-lint site_validator watir-webdriver
 
 # node/iojs environment
 brew install nvm
-export NVM_DIR=~/.nvm
+export NVM_DIR="${HOME}/.nvm"
 source "$(brew --prefix nvm)/nvm.sh"
 nvm install iojs
+nvm alias default iojs
 # install some packages
 npm install --global browser-sync bower divshot-cli gitignore harp html2jade imageoptim-cli jshint markdown-live nightmare pageres
