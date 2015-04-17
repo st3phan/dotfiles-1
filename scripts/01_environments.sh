a2b866514e304147ec0a0168e7b32a8e3e34861d
Vítor Galvão <info@vitorgalvao.com>
2015-04-17 22:16:23 +0100

    install python 2 and 3

--- scripts/01_environments.sh
+++ scripts/01_environments.sh
@@ -9,10 +9,12 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 
 # python environment
 brew install pyenv
-python_version=$(pyenv install --list | tr -d ' ' | grep '^2' | grep --invert-match 'dev\|a' | tail -1)
-pyenv install "${python_version}"
+python2=$(pyenv install --list | tr -d ' ' | grep '^2' | grep --invert-match 'dev\|a' | tail -1)
+python_latest=$(pyenv install --list | tr -d ' ' | grep '^\d' | grep --invert-match 'dev\|a' | tail -1)
+pyenv install "${python2}"
+pyenv install "${python_latest}"
 if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
-pyenv global "${python_version}"
+pyenv global "${python2}"
 # install some eggs
 pip install livestreamer subliminal
 
