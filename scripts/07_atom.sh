8959db12607ce62be3efea68952bd2b45132da85
Vítor Galvão <info@vitorgalvao.com>
2015-05-02 03:20:58 +0100

    changed some packages

--- scripts/07_atom.sh
+++ scripts/07_atom.sh
@@ -1,5 +1,5 @@
 # packages
-apm install atom-css-comb autocomplete-plus autocomplete-snippets color-picker dash esformatter git-plus javascript-snippets language-git language-jade linter linter-jshint linter-rubocop linter-scss-lint linter-shellcheck merge-conflicts processing processing-language seeing-is-believing vim-mode
+apm install atom-css-comb autocomplete-plus autocomplete-snippets dash esformatter git-plus language-git language-jade language-swift linter linter-jshint linter-rubocop linter-scss-lint linter-shellcheck linter-swiftc merge-conflicts processing processing-language seeing-is-believing swift-playground vim-mode
 
 # themes
 apm install spacegray-dark-ui
