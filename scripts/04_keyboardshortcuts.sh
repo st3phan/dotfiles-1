08507611f54aa0443b0e3b67f072c642de439471
Vítor Galvão <info@vitorgalvao.com>
2015-05-04 03:34:50 +0100

    commented global shortcuts

--- scripts/04_keyboardshortcuts.sh
+++ scripts/04_keyboardshortcuts.sh
@@ -3,10 +3,7 @@
 # read more at https://web.archive.org/web/20140810142907/http://hints.macworld.com/article.php?story=20131123074223584
 
 # Global
-defaults write -g NSUserKeyEquivalents '{
-  "Paste and Match Style"="@v";
-  "Paste"="@~$v";
-}'
+# defaults write -g NSUserKeyEquivalents '{}'
 
 # Tweetbot
 defaults write com.tapbots.TweetbotMac NSUserKeyEquivalents '{
