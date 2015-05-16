8ad410a5631f4eb461b16a39d4971685ecc72fb4
Vítor Galvão <info@vitorgalvao.com>
2015-05-17 00:58:53 +0100

    -jhsint +eslint

--- scripts/07_atom.sh
+++ scripts/07_atom.sh
@@ -1,2 +1,2 @@
 # packages
-apm install atom-css-comb dash esformatter git-plus language-jade language-swift linter linter-jshint linter-rubocop linter-scss-lint linter-shellcheck linter-swiftc merge-conflicts processing processing-language seeing-is-believing swift-playground vim-mode
+apm install atom-css-comb dash esformatter git-plus language-jade language-swift linter linter-eslint linter-rubocop linter-scss-lint linter-shellcheck linter-swiftc merge-conflicts processing processing-language seeing-is-believing swift-playground vim-mode
