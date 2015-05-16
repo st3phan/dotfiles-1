51cdb9c1490533b9bcbccf91c5a683293c7236e4
Vítor Galvão <info@vitorgalvao.com>
2015-05-16 19:53:38 +0100

    atom: removed some packages

--- scripts/07_atom.sh
+++ scripts/07_atom.sh
@@ -1,5 +1,5 @@
 # packages
-apm install atom-css-comb autocomplete-plus autocomplete-snippets dash esformatter git-plus language-git language-jade language-swift linter linter-jshint linter-rubocop linter-scss-lint linter-shellcheck linter-swiftc merge-conflicts processing processing-language seeing-is-believing swift-playground vim-mode
+apm install atom-css-comb dash esformatter git-plus language-jade language-swift linter linter-jshint linter-rubocop linter-scss-lint linter-shellcheck linter-swiftc merge-conflicts processing processing-language seeing-is-believing swift-playground vim-mode
 
 # themes
 apm install spacegray-dark-ui
