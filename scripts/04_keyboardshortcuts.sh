db75467f981105fa1eda922ea21e5d8bc4587a63
Vítor Galvão <info@vitorgalvao.com>
2015-05-17 02:34:47 +0100

    remove keybindings

--- scripts/04_keyboardshortcuts.sh
+++ scripts/04_keyboardshortcuts.sh
@@ -15,37 +15,3 @@ defaults write com.tapbots.TweetbotMac NSUserKeyEquivalents '{
   "Open Links"="l";
   "Send to Pinboard"="$l";
 }'
-
-# Custom keyboard shortcuts for input fields
-# read more at http://www.hcs.harvard.edu/~jrus/site/selectors.html and https://github.com/ttscoff/KeyBindings/blob/master/DefaultKeyBinding.dict
-cat << EOF > "${HOME}/Library/KeyBindings/DefaultKeyBinding.dict"
-{
-  // new paragraph from anywhere (⌘↩)
-  "@\U000D" = (moveToEndOfParagraph:, insertNewline:, insertNewline:);
-  // new paragraph above (⌘⇧↩)
-  "@$\U000D" = (moveToBeginningOfParagraph:, moveLeft:, insertNewline:, moveLeft:, insertNewline:);
-
-  // select entire line/paragraph
-  "~S" = (selectParagraph:);
-  // select paragraph excluding leading/trailing whitespace (same as ^$@\UF701)
-  "~s" = (moveToEndOfParagraph:, insertText:, " ", selectWord:, moveBackward:, moveToBeginningOfParagraphAndModifySelection:, moveWordForwardAndModifySelection:, moveWordBackwardAndModifySelection:);
-  // delete line/paragraph
-  "~d" = (selectParagraph:, delete:, insertNewline, moveLeft:);
-  // copy paragraph
-  "~y" = (setMark:, moveToBeginningOfParagraph:, moveToEndOfParagraphAndModifySelection:, copy:, swapWithMark:);
-  // cut paragraph
-  "~x" = (moveToBeginningOfParagraph:, moveToEndOfParagraphAndModifySelection:, cut:);
-  // paste paragraph below
-  "~p" = (moveToEndOfParagraph:,moveRight:,insertNewline:,moveLeft:, paste:);
-  // paste paragraph above
-  "~P" = (moveToBeginningOfParagraph:, moveLeft:, paste:);
-
-  // set mark
-  "~m" = (setMark:);
-  // jump to mark
-  "~'" = (swapWithMark:, centerSelectionInVisibleArea:);
-
-  // titlecase word
-  "~t" = (capitalizeWord:, moveWordForward:, moveWordBackward:);
-}
-EOF
