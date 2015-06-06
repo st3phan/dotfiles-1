e1d372d9dbfcab7c84c273d66f684a8dc357015b
Vítor Galvão <info@vitorgalvao.com>
2015-06-06 20:13:39 +0100

    removed swift

--- scripts/07_atom.sh
+++ scripts/07_atom.sh
@@ -1,5 +1,5 @@
 # packages
-apm install atom-css-comb color-picker dash esformatter git-plus language-jade language-swift linter linter-eslint linter-rubocop linter-scss-lint linter-shellcheck linter-swiftc merge-conflicts pigments seeing-is-believing swift-playground vim-mode
+apm install atom-css-comb color-picker dash esformatter git-plus language-jade linter linter-eslint linter-rubocop linter-scss-lint linter-shellcheck merge-conflicts pigments seeing-is-believing vim-mode
 
 # themes and syntaxes
 apm install peacock-syntax seti-syntax seti-ui
