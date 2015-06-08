dd47cdccc8a179c224d585b684263dd0a19911c9
Vítor Galvão <info@vitorgalvao.com>
2015-06-08 11:55:13 +0100

    added atom-alignment

--- scripts/07_atom.sh
+++ scripts/07_atom.sh
@@ -1,5 +1,5 @@
 # packages
-apm install atom-css-comb color-picker dash esformatter git-plus language-jade linter linter-eslint linter-rubocop linter-scss-lint linter-shellcheck merge-conflicts pigments seeing-is-believing vim-mode
+apm install atom-alignment atom-css-comb color-picker dash esformatter git-plus language-jade linter linter-eslint linter-rubocop linter-scss-lint linter-shellcheck merge-conflicts pigments seeing-is-believing vim-mode
 
 # themes and syntaxes
 apm install peacock-syntax seti-syntax seti-ui
