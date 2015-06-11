6814ea568478812c48a993a8c83dd853c44c01a4
Vítor Galvão <info@vitorgalvao.com>
2015-06-11 23:27:00 +0100

    removed hexels

--- scripts/02_apps.sh
+++ scripts/02_apps.sh
@@ -21,7 +21,7 @@ mkdir -p /opt/homebrew-cask/Caskroom
 sudo chown -R ${USER}:staff /opt/homebrew-cask
 
 # install apps
-brew cask install --appdir="/Applications" alfred awareness bartender bettertouchtool dropbox enjoyable flux google-chrome hexels imageoptim iterm2 jadengeller-helium keka key-codes lunchy macaw openemu p5 processing shotcut spectacle steam textexpander torbrowser transmission whiskey yacreader
+brew cask install --appdir="/Applications" alfred awareness bartender bettertouchtool dropbox enjoyable flux google-chrome imageoptim iterm2 jadengeller-helium keka key-codes lunchy macaw openemu p5 processing shotcut spectacle steam textexpander torbrowser transmission whiskey yacreader
 
 # install alternative versions
 brew tap caskroom/versions
