e5de30402ef1ff639a20e6b40798f7832f4a0276
Vítor Galvão <info@vitorgalvao.com>
2015-07-28 13:55:43 +0100

    07: -seti-syntax

--- scripts/07_atom.sh
+++ scripts/07_atom.sh
@@ -2,4 +2,4 @@
 apm install atom-alignment atom-css-comb color-picker dash esformatter git-plus highlight-line language-jade linter linter-eslint linter-rubocop linter-shellcheck merge-conflicts pigments seeing-is-believing vim-mode vim-surround
 
 # themes and syntaxes
-apm install peacock-syntax seti-syntax seti-ui
+apm install peacock-syntax seti-ui
