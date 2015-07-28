7fbdfbc7918c068af0fe2f149aa2575d8d4cd62f
Vítor Galvão <info@vitorgalvao.com>
2015-07-28 13:53:24 +0100

    07: remove scss linter

--- scripts/07_atom.sh
+++ scripts/07_atom.sh
@@ -1,5 +1,5 @@
 # packages
-apm install atom-alignment atom-css-comb color-picker dash esformatter git-plus highlight-line language-jade linter linter-eslint linter-rubocop linter-scss-lint linter-shellcheck merge-conflicts pigments seeing-is-believing vim-mode vim-surround
+apm install atom-alignment atom-css-comb color-picker dash esformatter git-plus highlight-line language-jade linter linter-eslint linter-rubocop linter-shellcheck merge-conflicts pigments seeing-is-believing vim-mode vim-surround
 
 # themes and syntaxes
 apm install peacock-syntax seti-syntax seti-ui
