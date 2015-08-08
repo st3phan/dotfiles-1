d0a1b0bac52f8aa0b4c2843459faa6e509a75da7
Vítor Galvão <info@vitorgalvao.com>
2015-08-08 21:10:33 +0100

    atom: option to set PATH to the current one

--- scripts/07_atom.sh
+++ scripts/07_atom.sh
@@ -3,3 +3,10 @@ apm install atom-alignment atom-css-comb color-picker dash esformatter git-plus 
 
 # themes and syntaxes
 apm install peacock-syntax seti-ui
+
+# set the correct PATH in atom
+atom_init="${HOME}/.atom/init.coffee"
+mkdir -p "${HOME}/.atom"
+curl --silent --output "${atom_init}" 'https://raw.githubusercontent.com/atom/atom/master/dot-atom/init.coffee' # get the default init.coffee
+echo '\n#set PATH' >> "${atom_init}" # add comment we will use to grab on, every subsequent time we want to update PATH
+sed -i '' "s|.*# set PATH|process.env.PATH = '$PATH' # set PATH|" "${HOME}/.atom/init.coffee" # update PATH in atom
