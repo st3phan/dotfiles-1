add1af6323c8553770fab7dca7581d2232bbdcc3
Vítor Galvão <info@vitorgalvao.com>
2015-09-22 00:26:48 +0100

    added css linter

--- scripts/01_environments.sh
+++ scripts/01_environments.sh
@@ -35,4 +35,4 @@ source "$(brew --prefix nvm)/nvm.sh"
 nvm install node
 nvm alias default node
 # install some packages
-npm install --global divshot-cli electron-prebuilt eslint gitignore html2jade jsonlint markdown-live nightmare pageres-cli ttystudio watch
+npm install --global csslint divshot-cli electron-prebuilt eslint gitignore html2jade jsonlint markdown-live nightmare pageres-cli ttystudio watch
