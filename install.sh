22786a82f1cac2d2c760dafe9de26e3bc2c3b826
Vítor Galvão <info@vitorgalvao.com>
2015-09-26 19:34:09 +0100

    added launchagents

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 get_everything() {
   git clone https://github.com/vitorgalvao/dotfiles.git /tmp/dotfiles/
 
+  # move files that'll need manual action
+  mv "/tmp/dotfiles/files/" "${HOME}/Desktop/"
+
   # concatenate all shell scripts together, so things like variables can be reused
   cat /tmp/dotfiles/scripts/*.sh > /tmp/script
   bash /tmp/script
 
   # run ruby scripts
   ruby /tmp/dotfiles/scripts/07_chrome_extensions.rb
-
-  # move files that'll need manual action
-  mv "/tmp/dotfiles/files/" "${HOME}/Desktop/"
 }
 
 get_everything
