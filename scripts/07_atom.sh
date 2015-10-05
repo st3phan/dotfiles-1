aea8ee61f1649280026bbb0209e7c828c8ba8433
Vítor Galvão <info@vitorgalvao.com>
2015-10-05 01:46:08 +0100

    07_atom.sh: use atom_init in susbtitution

--- scripts/07_atom.sh
+++ scripts/07_atom.sh
@@ -9,4 +9,4 @@ atom_init="${HOME}/.atom/init.coffee"
 mkdir -p "${HOME}/.atom"
 curl --silent --output "${atom_init}" 'https://raw.githubusercontent.com/atom/atom/master/dot-atom/init.coffee' # get the default init.coffee
 echo '\n#set PATH' >> "${atom_init}" # add comment we will use to grab on, every subsequent time we want to update PATH
-sed -i '' "s|.*# set PATH|process.env.PATH = '$PATH' # set PATH|" "${HOME}/.atom/init.coffee" # update PATH in atom
+sed -i '' "s|.*# set PATH|process.env.PATH = '$PATH' # set PATH|" "${atom_init}" # update PATH in atom
