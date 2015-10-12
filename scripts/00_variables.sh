7d0199b6f31920b571e475615104ff7cbd75e78c
Vítor Galvão <info@vitorgalvao.com>
2015-10-12 18:57:46 +0100

    Update 00_variables.sh

--- scripts/00_variables.sh
+++ scripts/00_variables.sh
@@ -1,2 +1,4 @@
 read -p 'Insert your email address (needed for the lock screen message) >' email
 read -p 'Insert your telephone number (needed for the lock screen message) >' telephone
+read -p 'Insert your github username >' github_username
+read -p 'Insert your github email >' github_email
