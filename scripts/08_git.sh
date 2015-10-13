9c515ac6e93fecbd4a457eb6fbcb9ed6d04f6c21
Vítor Galvão <info@vitorgalvao.com>
2015-10-13 21:39:24 +0100

    use full name with git

--- scripts/08_git.sh
+++ scripts/08_git.sh
@@ -1,5 +1,5 @@
 # configure git
-echo "[user]\n\tname = ${github_username}\n\temail = ${github_email}" > "${HOME}/.gitconfig"
+echo "[user]\n\tname = ${name}\n\temail = ${github_email}" > "${HOME}/.gitconfig"
 
 # configure ghi
 /usr/local/bin/ghi config --auth "${github_username}"
