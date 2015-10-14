c63e6db019eb875e7059b6c7bfe8f96e38a753c5
Vítor Galvão <info@vitorgalvao.com>
2015-10-14 18:43:06 +0100

    08_git.sh: corrected echo command

--- scripts/08_git.sh
+++ scripts/08_git.sh
@@ -1,5 +1,5 @@
 # configure git
-echo "[user]\n\tname = ${name}\n\temail = ${github_email}" > "${HOME}/.gitconfig"
+echo -e "[user]\n\tname = ${name}\n\temail = ${github_email}" > "${HOME}/.gitconfig"
 
 # configure ghi
 /usr/local/bin/ghi config --auth "${github_username}"
