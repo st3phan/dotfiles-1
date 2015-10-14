0e09772f3e2e9f4ff728b507f1efac892c2b8cc4
Vítor Galvão <info@vitorgalvao.com>
2015-10-14 18:54:54 +0100

    03_osx.sh: dashboard out

--- scripts/03_osx.sh
+++ scripts/03_osx.sh
@@ -137,9 +137,6 @@ defaults write com.apple.dock orientation -string left
 # Disable Dock icon magnification
 defaults write com.apple.dock magnification -bool false
 
-# Don’t show Dashboard as a Space
-defaults write com.apple.dock dashboard-in-overlay -bool true
-
 # Don’t automatically rearrange Spaces based on most recent use
 defaults write com.apple.dock mru-spaces -bool false
 
