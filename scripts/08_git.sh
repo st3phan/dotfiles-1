32efad0d1a9766ac99925a5b23cb10984ad2e23b
Vítor Galvão <info@vitorgalvao.com>
2015-10-14 21:34:13 +0100

    use osxkeychain

--- scripts/08_git.sh
+++ scripts/08_git.sh
@@ -1,5 +1,7 @@
 # configure git
 echo -e "[user]\n\tname = ${name}\n\temail = ${github_email}" > "${HOME}/.gitconfig"
 
+git config --global credential.helper osxkeychain
+
 # configure ghi
 ghi config --auth "${github_username}"
