c2d8c009c82eed05d1dd5bdcd168774c6afe80f3
Vítor Galvão <info@vitorgalvao.com>
2016-06-03 16:43:02 +0100

    cosmetic restructuring in ask_details

--- scripts/00_setup.sh
+++ scripts/00_setup.sh
@@ -16,18 +16,18 @@ initial_setup() {
   readonly helper_files='/tmp/dotfiles-master/files'
   readonly post_install_files="${HOME}/Desktop/post_install_files"
   readonly post_install_script="${HOME}/Desktop/post_install_script.sh"
+}
 
+ask_details() {
   # ask for the administrator password upfront, for commands that require 'sudo'
   clear
   bold_echo 'Insert the "sudo" password now (will not be echoed).'
   until sudo -n true 2> /dev/null; do # if password is wrong, keep asking
-    read -s -p "Password:" sudo_password
+    read -s -p 'Password: ' sudo_password
     echo
     sudo -S -v <<< "${sudo_password}" 2> /dev/null
   done
-}
 
-ask_details() {
   # ask for Mac App Store password
   clear
   bold_echo 'Insert your Mac App Store details to install apps.'
@@ -36,14 +36,14 @@ ask_details() {
 
   clear
   bold_echo 'Some details to be used when configuring git:'
-  read -p 'First and last names > ' name
-  read -p 'Github username > ' github_username
-  read -p 'Github email > ' github_email
+  read -p 'First and last names: ' name
+  read -p 'Github username: ' github_username
+  read -p 'Github email: ' github_email
 
   clear
   bold_echo 'Some contact information to be set in the lock screen:'
-  read -p 'Email address > ' email
-  read -p 'Telephone number > ' telephone
+  read -p 'Email address: ' email
+  read -p 'Telephone number: ' telephone
   sudo -S defaults write /Library/Preferences/com.apple.loginwindow LoginwindowText "Email: ${email}\nTel: ${telephone}" <<< "${sudo_password}" 2> /dev/null
 }
 
