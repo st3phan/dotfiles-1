f29801e7d2dec622a0d7d03ac278c500630bab14
Vítor Galvão <info@vitorgalvao.com>
2016-10-12 12:25:04 +0100

    added rmlint

--- scripts/02_apps.sh
+++ scripts/02_apps.sh
@@ -3,7 +3,7 @@ install_brew_apps() {
   brew install sox --with-flac --with-lame --with-libvorbis
   brew install zsh --without-etcdir
 
-  brew install aria2 asciinema asciinema2gif cpulimit duti eye-d3 ghi git git-ftp haskell-stack hr httpie imagemagick jq livestreamer mediainfo mkvtoolnix mp4v2 onepass phantomjs pup ripgrep shellcheck subliminal trash tree youtube-dl z
+  brew install aria2 asciinema asciinema2gif cpulimit duti eye-d3 ghi git git-ftp haskell-stack hr httpie imagemagick jq livestreamer mediainfo mkvtoolnix mp4v2 onepass phantomjs pup ripgrep rmlint shellcheck subliminal trash tree youtube-dl z
 
   # install HEAD apps
   brew install --HEAD neovim/neovim/neovim
