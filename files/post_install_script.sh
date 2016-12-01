10c01b3f2191192d6b165fc33f7652218c768fac
Vítor Galvão <info@vitorgalvao.com>
2016-12-01 21:51:18 +0000

    post_install: TTS is now TextToSpeech in com.apple.preference.universalaccess

--- files/post_install_script.sh
+++ files/post_install_script.sh
@@ -197,7 +197,7 @@ request_preferences 'Under "Trackpad Options…", enable three finger drag.'
 preferences_pane_anchor 'Dictation' 'com.apple.preference.keyboard'
 request_preferences 'Turn on enhanced dictation and download other languages.'
 
-preferences_pane_anchor 'TTS' 'com.apple.preference.speech'
+preferences_pane_anchor 'TextToSpeech' 'com.apple.preference.universalaccess'
 request_preferences 'Download and keep only "Ava" and "Joana" voices.'
 
 # chrome extentions
