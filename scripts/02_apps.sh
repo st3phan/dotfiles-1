f6a12a0010e35253ccbcdd7a0312d28186378bf1
Vítor Galvão <info@vitorgalvao.com>
2017-03-27 20:29:57 +0100

    remove flux

--- scripts/02_apps.sh
+++ scripts/02_apps.sh
@@ -21,7 +21,7 @@ install_brew_apps() {
 
 install_cask_apps() {
   renew_sudo # to make the Caskroom on first install
-  brew cask install alfred appearin apple-events atom bartender bettertouchtool calibre dolphin dropbox electron-api-demos flux fog gifloopcoder gitup google-chrome imageoptim imitone iterm2 keka oversight processing shotcut spectacle steam torbrowser transmission veertu-desktop whale whatsyoursign wwdc yacreader
+  brew cask install alfred appearin apple-events atom bartender bettertouchtool calibre dolphin dropbox electron-api-demos fog gifloopcoder gitup google-chrome imageoptim imitone iterm2 keka oversight processing shotcut spectacle steam torbrowser transmission veertu-desktop whale whatsyoursign wwdc yacreader
 
   brew cask install pokerstars --language='PT'
 
