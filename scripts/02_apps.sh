1f569e4dc2cc96c1f3cf3f4ea2ca77ed6b425384
Vítor Galvão <info@vitorgalvao.com>
2017-08-24 18:32:16 +0100

    removed everything anka an veertu

--- scripts/02_apps.sh
+++ scripts/02_apps.sh
@@ -21,7 +21,7 @@ install_brew_apps() {
 
 install_cask_apps() {
   renew_sudo # to make the Caskroom on first install
-  brew cask install alfred anka-flow apple-events atom bartender bettertouchtool blockblock calibre dolphin dropbox electron-api-demos fog gifloopcoder gitup google-chrome imageoptim imitone iterm2 keka oversight processing shotcut spectacle steam torbrowser transmission veertu-desktop whale whatsyoursign wwdc yacreader
+  brew cask install alfred apple-events atom bartender bettertouchtool blockblock calibre dolphin dropbox electron-api-demos fog gifloopcoder gitup google-chrome imageoptim imitone iterm2 keka oversight processing shotcut spectacle steam torbrowser transmission whale whatsyoursign wwdc yacreader
 
   brew cask install pokerstars --language='PT'
 
